b7caecea899eaff4012e8d4f1090f649d4a0f30d
Ami Iosef Ustilovsky Stifel <ami.ustilovsky@darrow.ai>
2026-01-02 11:59:23 +0200

    Add news-article collection type for press coverage

---
+++ src/api/news-article/routes/news-article.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * news-article router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::news-article.news-article');
+
